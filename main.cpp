b05d5b3660765c259d8bd572d16c00bde5d007af
Яна Пастернак <yana@MacBook-Air-Yana.local>
2024-05-17 21:07:36 +0200

    add cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hackathon
+//
+//  Created by Яна Пастернак on 17.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
